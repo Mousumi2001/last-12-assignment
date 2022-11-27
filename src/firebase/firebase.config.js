762d9acfd6e17a83dd71d9ed5d7d8d7d9cd6873c
Mousumi2001 <am6555264@gmail.com>
2022-11-27 21:51:43 +0600

    create env.local file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDEnHsJ9YZFjmyg-23K9KQHW1ZXmc1b458",
-    authDomain: "last-assignment-2e3dd.firebaseapp.com",
-    projectId: "last-assignment-2e3dd",
-    storageBucket: "last-assignment-2e3dd.appspot.com",
-    messagingSenderId: "338457101442",
-    appId: "1:338457101442:web:d67423154fa3ed32e9209b"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
